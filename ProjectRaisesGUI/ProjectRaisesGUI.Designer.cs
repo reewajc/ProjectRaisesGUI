7dc4b3458f3fbfe8e14d59f42b53de8d10b491e4
reewajc <37374398+reewajc@users.noreply.github.com>
2020-10-29 23:02:42 -0500

    Add project files.

---
+++ ProjectRaisesGUI/ProjectRaisesGUI.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace ProjectRaisesGUI
+{
+    partial class ProjectRaisesGUI
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.txtSalaryInput = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblDisplaySalary = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-185, -57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.Location = new System.Drawing.Point(48, 46);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(254, 36);
+            this.lblInstruction.TabIndex = 1;
+            this.lblInstruction.Text = "Please enter your current salary:";
+            // 
+            // txtSalaryInput
+            // 
+            this.txtSalaryInput.AcceptsReturn = true;
+            this.txtSalaryInput.Location = new System.Drawing.Point(391, 43);
+            this.txtSalaryInput.Name = "txtSalaryInput";
+            this.txtSalaryInput.Size = new System.Drawing.Size(182, 27);
+            this.txtSalaryInput.TabIndex = 2;
+            this.txtSalaryInput.TextChanged += new System.EventHandler(this.txtSalaryInput_TextChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(246, 268);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(166, 29);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblDisplaySalary
+            // 
+            this.lblDisplaySalary.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblDisplaySalary.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblDisplaySalary.Location = new System.Drawing.Point(165, 118);
+            this.lblDisplaySalary.Name = "lblDisplaySalary";
+            this.lblDisplaySalary.Size = new System.Drawing.Size(339, 109);
+            this.lblDisplaySalary.TabIndex = 4;
+            this.lblDisplaySalary.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblDisplaySalary.Visible = false;
+            // 
+            // ProjectRaisesGUI
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 336);
+            this.Controls.Add(this.lblDisplaySalary);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtSalaryInput);
+            this.Controls.Add(this.lblInstruction);
+            this.Controls.Add(this.label1);
+            this.Name = "ProjectRaisesGUI";
+            this.Text = "Next Year Salary Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.TextBox txtSalaryInput;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblDisplaySalary;
+    }
+}
+
---
+++ ProjectRaisesGUI/ProjectRaisesGUI.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Author: Ram Chapagai
+ * Date: 10/29/2020
+ * Company: Bellevue
+ * Description: Salary projection app
+ * 
+ */
+
+namespace ProjectRaisesGUI
+{
+    public partial class ProjectRaisesGUI : Form
+    {
+        //variable to hold salary for next year
+        double salaryNextYear = 0;
+        public ProjectRaisesGUI()
+        {
+            InitializeComponent();
+        }
+
+        //Calculate button click event handler
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double currentSalary;
+            //Check whether the user entered a value or not in text box
+            if(txtSalaryInput.Text.Trim() != string.Empty )
+            {
+                try
+                {
+                    currentSalary = Convert.ToDouble(txtSalaryInput.Text);
+                    salaryNextYear = currentSalary + (0.4 * currentSalary);
+                    lblDisplaySalary.Text = "Your next year salary with 4% increase is: " + salaryNextYear.ToString("C");
+                    lblDisplaySalary.Visible = true;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Please enter a valid salary", "Invalid Salary");
+                    txtSalaryInput.Clear();
+                    txtSalaryInput.Focus();
+                  
+                }
+              
+            }
+            //If no value was entered in the text field, dispaly error via alert
+            else
+            {
+                MessageBox.Show("You haven't enter your salary", "Please try it again!");
+                txtSalaryInput.Focus();
+            }
+            
+           
+        }
+
+        //When the input changes in the text field the event is triggered.
+        private void txtSalaryInput_TextChanged(object sender, EventArgs e)
+        {
+            lblDisplaySalary.Text = " ";
+            lblDisplaySalary.Visible = false;
+        }
+    }
+}
